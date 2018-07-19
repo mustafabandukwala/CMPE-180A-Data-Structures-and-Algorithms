54866cbae339a8f55d9a565555c24bfec0121133
Mustafa Bandukwala <bandukwalamustafa@gmail.com>
2018-07-18 21:44:54 -0700

    Create SortedVector.h

---
+++ Assignments/Assignment 7/SortedVector.h
@@ -0,0 +1,82 @@
+/*****************************************************************************
+ * Name        : SortedVector.h
+ * Author      : Mustafa Bandukwala
+ * Version     : 1.0
+ * Description : File which contains various functions related to SortedVector
+ *****************************************************************************/
+
+#ifndef SORTEDVECTOR_H_
+#define SORTEDVECTOR_H_
+
+#include <vector>
+#include <iterator>
+#include "Node.h"
+
+using namespace std;
+
+/**
+ * A sorted vector of Node objects.
+ */
+class SortedVector : public vector<Node>
+{
+public:
+
+    vector<Node> sortedVector;
+
+    /**
+     * Defaut constructor.
+     * Reset the global Node counters.
+     */
+    SortedVector();
+
+    /**
+     * Destructor.
+     * Reset the global Node counters.
+     */
+    virtual ~SortedVector();
+
+    /**
+     * Insert a new node at the beginning of the vector.
+     * @param value the new node's value.
+     */
+    void prepend(const long value);
+
+    /**
+     * Append a new node at the end of the vector.
+     * @param value the new node's value.
+     */
+    void append(const long value);
+
+    /**
+     * Insert a new node into the vector at the
+     * appropriate position to keep the vector sorted.
+     * @param value the value of the node to insert.
+     */
+    void inserts(const long value);
+
+    /**
+     * Remove a node.
+     * @param index the index of the node to remove.
+     */
+    void remove(const int index);
+
+    /**
+     * Reverse in place the order of the nodes of the vector.
+     */
+    void reverse();
+
+    /**
+     * Check that the nodes are sorted.
+     * @return true if sorted, false if not.
+     */
+    bool check() const;
+
+    /**
+     * Check that the nodes are reverse sorted.
+     * @return true if reverse sorted, false if not.
+     */
+    bool check_reversed() const;
+
+};
+
+#endif /* SORTEDVECTOR_H_ */
