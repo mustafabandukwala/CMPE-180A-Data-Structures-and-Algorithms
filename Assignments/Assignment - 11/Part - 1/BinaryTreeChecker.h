800628b64fc056a88fe7b19cda5afd8e6c4364a2
Mustafa Bandukwala <bandukwalamustafa@gmail.com>
2018-08-01 22:10:51 -0700

    Create BinaryTreeChecker.h

---
+++ Assignments/Assignment - 11/Part - 1/BinaryTreeChecker.h
@@ -0,0 +1,78 @@
+/**********************************************************************************
+ * Name        : BinaryTreeChecker.h
+ * Author      : Mustafa Bandukwala
+ * Version     : 1.0
+ * Description : File which contains various functions related to BinaryTreeChecker
+ **********************************************************************************/
+
+#ifndef BINARY_TREE_CHECKER_H
+#define BINARY_TREE_CHECKER_H
+
+#include <iostream>
+#include <set>
+#include <iterator>
+#include <algorithm>
+#include "BinaryNode.h"
+#include "BinarySearchTree.h"
+
+class BinaryTreeChecker
+{
+public:
+    static const int NO_ERROR          =  0;
+    static const int DATA_MISMATCH     = -1;
+    static const int INSUFFICIENT_DATA = -2;
+    static const int REMAINING_DATA    = -3;
+
+    /**
+     * Default constructor.
+     */
+    BinaryTreeChecker(BinarySearchTree& tree);
+
+    /**
+     * Destructor.
+     */
+    virtual ~BinaryTreeChecker();
+
+    /**
+     * Clear the cache.
+     */
+    void clear();
+
+    /**
+     * Add a tree node's data item to the cache.
+     * @param data the data item to add.
+     */
+    void add(const int data);
+
+    /**
+     * Remove a data item from the cache.
+     * @param data the data item to remove.
+     */
+    void remove(const int data);
+
+    /**
+     * Check the tree's current data items against the cache.
+     * @param dump true to dump the data items in the tree and in the cache.
+     * @return a status code.
+     */
+    int check(const bool dump);
+
+private:
+    BinarySearchTree& tree;  // the tree to check
+    set<int> cache;          // the cache of data items
+    set<int> data;           // the tree's data items
+
+    /**
+     * Do an inorder walk of the tree to collect its data items.
+     * @param node the root node of the subtree to walk (initially the tree root).
+     */
+    void walk(BinaryNode *node);
+
+    /**
+     * Compare the tree's data items against the data items in the cache.
+     * @return a status code.
+     */
+    int compare(const bool dump) const;
+};
+
+#endif // BINARY_TREE_CHECKER_H
