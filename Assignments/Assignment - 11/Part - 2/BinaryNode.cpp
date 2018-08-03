aab1f7b82c6ba9c0428b1d3b8c32025aaedcbfc6
Mustafa Bandukwala <bandukwalamustafa@gmail.com>
2018-08-03 13:24:25 -0700

    Create BinaryNode.cpp

---
+++ Assignments/Assignment - 11/Part - 2/BinaryNode.cpp
@@ -0,0 +1,22 @@
+/***************************************************************************
+ * Name        : BinaryNode.cpp
+ * Author      : Mustafa Bandukwala
+ * Version     : 1.0
+ * Description : File which contains various functions related to BinaryNode
+ ***************************************************************************/
+
+#include "BinaryNode.h"
+
+BinaryNode::BinaryNode(const int data)
+    : data(data), height(0), left(nullptr), right(nullptr)
+{
+}
+
+BinaryNode::BinaryNode(const int data, BinaryNode *left, BinaryNode *right)
+  : data(data), height(0), left(left), right(right)
+{
+}
+
+BinaryNode::~BinaryNode()
+{
+}
